c2b906531e1dc30a38d9fd2e7b8422948b230d30
Nasir-Hussain786 <starbro646@gmail.com>
2026-06-15 18:37:21 +0500

    feat: add search button and enter key event listeners

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ let allRepos = [];
 
 // Default sorting
 let sortBy = "stars";
+
+// Search button click
+document.getElementById("searchBtn").addEventListener("click", searchUser);
+
+// Enter key press
+document.getElementById("usernameInput").addEventListener("keypress", function(e) {
+    if (e.key === "Enter") searchUser();
+});
